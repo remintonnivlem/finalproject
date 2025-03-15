493efb58e6e964d2dfe2dcc62289b9fba8489261
remintonnivlem <remintonnivlem@gmail.com>
2025-03-15 08:09:23 +0000

    Add 9.cpp

---
+++ src/9.cpp
@@ -0,0 +1,13 @@
+// Example code to generate a random C++ code for your Final Project
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    // Generate a random number between 1 and 10
+    int num = std::rand() % 10 + 1;
+
+    // Print the result to the console
+    std::cout << "The random number is: " << num << "\n";
+
+    return 0;
+}
