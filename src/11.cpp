03577bacec8064a5f097311ac1abab399e77ef90
remintonnivlem <remintonnivlem@gmail.com>
2025-03-17 16:06:54 +0000

    Add 11.cpp

---
+++ src/11.cpp
@@ -0,0 +1,12 @@
+
+// This is a sample C++ code for a simple calculator program that takes two integers as input and returns their sum
+#include <iostream>
+using namespace std;
+int main() {
+    int num1, num2, sum;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    sum = num1 + num2;
+    cout << "Sum of the two numbers is: " << sum;
+    return 0;
+}
