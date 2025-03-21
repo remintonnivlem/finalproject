0c42da069bf09b7c5ede65d9f380dfd96a2b130f
remintonnivlem <remintonnivlem@gmail.com>
2025-03-21 02:28:56 +0000

    Add 15.cpp

---
+++ src/15.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 5;
+    if(i > 0) cout << "The number is greater than zero." << endl;
+    else cout << "The number is not greater than zero." << endl;
+}
