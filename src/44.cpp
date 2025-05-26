c3a3486d23d00d944f5a20fc1febe2e1e6d72cc0
remintonnivlem <remintonnivlem@gmail.com>
2025-05-26 12:48:22 +0000

    Add 44.cpp

---
+++ src/44.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    string name = "John";
+    float salary = 5000.5f;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Salary: $" << salary << endl;
+
+    return 0;
+}
